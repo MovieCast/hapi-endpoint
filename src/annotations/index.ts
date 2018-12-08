ba42576553e093123bad243f5fe27a8f622f1018
viction <26467584+victionn@users.noreply.github.com>
2018-12-08 19:32:08 +0000

    Update index.ts

--- src/annotations/index.ts
+++ src/annotations/index.ts
@@ -1,5 +1,6 @@
 export { Auth } from './Auth';
 export { Controller } from './Controller';
+export { Payload } from './Payload';
 export { Pre } from './Pre';
 export { Route } from './Route';
 export { Validate } from './Validate';
